0c61176c5aa772b46553526dc6223e499fa25c48
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2023-09-25 23:01:48 +0100

    First Commit

---
+++ src/main/java/com/learnspringboot/springdemo/student/StudentController.java
@@ -0,0 +1,23 @@
+package com.learnspringboot.springdemo.student;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+
+@GetMapping
+    public List<String> findAllStudents(){
+return  List.of(
+        "Ahmed",
+        "belhaj boubaker"
+);
+    }
+
+}
---
+++ src/test/java/com/learnspringboot/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
